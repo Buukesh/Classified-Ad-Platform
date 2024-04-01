e1a22b9a29bd6c3833887ed8fcd684153355729a
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-04-01 17:06:23 -0400

    homepage goes to signup if not logged in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
 import AdList from "../components/AdList.jsx";
 import NavBar from "../components/Navbar.jsx";
 import SearchBar from "../components/SearchBar.jsx";
+import useAppContext from "../context/useAppContext";
 
 const HomePage = () => {
+    const { token } = useAppContext();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // this runs when homepage loads
+        if (!token) {
+            // if token not set (not logged in), go to signup
+            navigate("/signup");
+        }
+    }, [token, navigate]);
+
     return (
         <section className="bg-black-800">
             <NavBar />
