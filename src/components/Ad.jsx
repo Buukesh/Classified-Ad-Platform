43ee6a666a4f5ef24ec84f8bde1015aafc5a0bf3
Buukesh <sukeshsivagnaruban@gmail.com>
2024-04-02 18:51:06 -0400

    Added price tag to listings

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -6,6 +6,13 @@ const Ad = ({ imgSrc, title, description }) => {
             <div className="card lg:card-side shadow-xl w-1/2 lg:w-1/2">
                 <img src={imgSrc} />
 
+                <div className="absolute top-2 right-2 bg-secondary px-4 py-1 rounded-full">
+                    {/* Currently using sample price */}
+                    <span className="text-lg font-semibold text-white">
+                        {"$100 (Sample)"}
+                    </span>
+                </div>
+
                 <div className="card-body bg-base-200">
                     <h2 className="card-title">{title}</h2>
                     <p>{description}</p>
@@ -19,6 +26,7 @@ Ad.propTypes = {
     imgSrc: PropTypes.string,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    price: PropTypes.string,
 };
 
 export default Ad;
