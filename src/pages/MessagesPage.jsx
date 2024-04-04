2e5bba83fca50a377cd90270cbfff6b6e56715e7
Bukesh <78332928+Buukesh@users.noreply.github.com>
2024-04-04 13:12:12 -0400

    Added get request for conversations

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -5,7 +5,7 @@ import NavBar from "../components/Navbar";
 import ChatSideBar from "../components/ChatSideBar";
 
 const MessagesPage = () => {
-    const [userConvos, setUserConvos] = useState([]);
+    const [convos, setConvos] = useState([]);
     // Default conversations
     const userConversations = [
         "Conversation 1",
@@ -13,30 +13,23 @@ const MessagesPage = () => {
         "Conversation 3",
     ];
     useEffect(() => {
-        const getUserConvos = async () => {
-            const token = localStorage.getItem('token') || '';
-            // Put API url here to fetch conversations
-            try {
-            const data = await fetchData("/api/messages", {
-                headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': `Bearer ${token}` // Include the authorization header
-                }
-              });
-            setUserConvos(data);
-            } catch (error) {
-                console.error("Failed to fetch conversations:", error);
-                // Handle error state appropriately, perhaps set an error message in state and display it
-            }
+        const getConvos = async () => {
+            const token = localStorage.getItem('token');
+            const data = await fetchData("/api/messages/conversations", {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${token}`
+            });
+            const adsArray = data.map(item => item.ad.title);
+            setConvos(adsArray);
         };
-        getUserConvos();
+        getConvos();
     }, []);
 
     return (
         <section>
             <NavBar />
             <div>
-                <ChatSideBar sidebarItems={userConversations} />
+                <ChatSideBar sidebarItems={convos} />
             </div>
         </section>
     );
