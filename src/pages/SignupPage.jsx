ccfa713b9a98943566f65af0f890f550c32d723c
Bukesh <78332928+Buukesh@users.noreply.github.com>
2024-04-06 05:13:18 -0400

    Changed linkto to login

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -192,10 +192,10 @@ const SignupPage = () => {
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                         Already have an account?{" "}
                         <Link
-                            to="/signup"
+                            to="/login"
                             className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                         >
-                            Sign up
+                            Log in
                         </Link>
                     </p>
                 </div>
