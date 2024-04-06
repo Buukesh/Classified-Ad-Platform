745c18638dac33294f3dae2a224bde15d5eae642
Bukesh <78332928+Buukesh@users.noreply.github.com>
2024-04-06 05:09:33 -0400

    Added link to for signup page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -191,12 +191,12 @@ const SignupPage = () => {
                     </div>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                         Already have an account?{" "}
-                        <a
-                            href="/login"
+                        <Link
+                            to="/signup"
                             className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                         >
-                            Sign in
-                        </a>
+                            Sign up
+                        </Link>
                     </p>
                 </div>
             </div>
