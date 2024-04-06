8f08da066a1e2326286605485031bf7accbe5040
Bukesh <78332928+Buukesh@users.noreply.github.com>
2024-04-06 03:59:29 -0400

    CORS_ORIGIN_ALLOW_ALL set to true

--- cap_backend/cap_backend/settings.py
+++ cap_backend/cap_backend/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['classified-ad-platform.onrender.com', 'localhost']
 
+CORS_ORIGIN_ALLOW_ALL = True # Set to True to allow all origins
 CORS_ALLOW_CREDENTIALS = True  # Set to True to allow cookies to be included in cross-origin requests
 
 # Application definition
