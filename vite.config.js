35286066836f21161049f8766738dd9429c54111
Bukesh <78332928+Buukesh@users.noreply.github.com>
2024-04-05 23:13:36 -0400

    target server link

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://classified-ad-platform.onrender.com/",
         changeOrigin: true,
       },
     },
