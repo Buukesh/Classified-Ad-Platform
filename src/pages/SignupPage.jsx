4cf7d76c9360895dd3354b35cde6469c69b9da3c
Bukesh <78332928+Buukesh@users.noreply.github.com>
2024-04-06 05:21:03 -0400

    Import link

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
+import { Link } from "react-router-dom";
+
 import { postData } from "../../utils";
 
 const SignupPage = () => {
